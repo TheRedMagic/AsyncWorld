54dc5efd9acec48d3dedb747dd88d7bbf239ddc1
lijnb <lijnbelt13@gmail.com>
2025-10-03 11:08:25 +0200

    feat(gradle): set-up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,26 @@
+import com.undefinedcreations.nova.ServerType
+
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+    id("com.undefinedcreations.nova") version "0.0.8"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "gg.undefinedaquatic"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    paperweight.paperDevBundle("1.21.9-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(23)
+tasks {
+    runServer {
+        serverType(ServerType.PAPERMC)
+        minecraftVersion("1.21.9")
+        acceptMojangEula()
+    }
 }
---
+++ src/main/kotlin/gg/undefinedaquatic/asyncWorld/AsyncWorld.kt
@@ -0,0 +1,6 @@
+package gg.undefinedaquatic.asyncWorld
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AsyncWorld : JavaPlugin() {
+}
